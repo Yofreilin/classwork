aba4438270120533db5aedd270a78d29e95f01bf
yofreilin <ygiraldoguillermo@bostonk12.org>
2025-01-09 11:44:25 -0500

    clase de 1/09

---
+++ 2024-2025/Python/Week_9/2025-01-09/if.py
@@ -0,0 +1,10 @@
+# Yofreilin
+# 2025-01-09
+
+num1=int(input('Enter a number: '))
+num2=int(input('Enter another number: '))
+
+if num1>num2:
+    print("The biggest number that I have seen so far is ",num1)
+else:
+    print("The biggest number that I have seen so far is ",num2)
