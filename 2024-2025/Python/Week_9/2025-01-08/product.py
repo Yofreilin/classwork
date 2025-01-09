9e0ca131319d246a12ed93b1fe016bae481a0835
yofreilin <ygiraldoguillermo@bostonk12.org>
2025-01-09 07:32:40 -0500

    clase de 1/08

---
+++ 2024-2025/Python/Week_9/2025-01-08/product.py
@@ -0,0 +1,10 @@
+# Yofreilin
+# 2025-01-08
+
+num1=int(input('Enter a number: '))
+num2=int(input('Enter another number: '))
+num3=int(input('Enter a third number: '))
+
+product = num1*num2*num3
+print(product)
+print(num1,"x",num2,"x",num3,"=",product)
