f793b896edda04c6611e6f8a819694da1769a9f0
yofreilin <ygiraldoguillermo@bostonk12.org>
2025-01-09 07:32:05 -0500

    clase de 1/07

---
+++ 2024-2025/Python/Week_9/2025-01-07/sum.py
@@ -0,0 +1,8 @@
+#Yofreilin
+#2025-01-07
+
+num1 = int(input('enter first number: '))
+Num2 =int(input('enter second number: '))
+
+sum = (num1 + Num2)
+print("the sum of", num1, "and", Num2, "is", sum)
