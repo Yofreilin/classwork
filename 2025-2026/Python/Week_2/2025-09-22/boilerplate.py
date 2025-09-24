452f41bc24e97b32c5b266376e175f7ca50c4718
410133 <Ygiraldoguillermo@bostonk12.org>
2025-09-24 09:39:06 -0400

    la clase de hoy

---
+++ 2025-2026/Python/Week_2/2025-09-22/boilerplate.py
@@ -0,0 +1,9 @@
+#Yofreilin
+#2025-09-22
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2025-2026/Python/Week_2/2025-09-24/boilerplate.py
@@ -0,0 +1,9 @@
+#Yofreilin
+#2025-09-24
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
