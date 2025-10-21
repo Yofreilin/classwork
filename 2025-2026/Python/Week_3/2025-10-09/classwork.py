10da6ea2d9aeb33afa123b885f45ce96b4ddd414
410133 <Ygiraldoguillermo@bostonk12.org>
2025-10-21 11:42:58 -0400

    clase de hoy

---
+++ 2025-2026/Python/Week_3/2025-10-09/classwork.py
@@ -0,0 +1,7 @@
+def main():
+    print("Pleas enter a number:6" )
+    answer=input("Here is the number that you chose:6" )
+    print("Chat says:" + answer)
+
+if __name__ == '__main__':
+    main()
