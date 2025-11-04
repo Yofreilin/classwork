d776da4a870a742a80c22c67e1944822f0b1f0f5
410133 <Ygiraldoguillermo@bostonk12.org>
2025-11-04 10:35:12 -0500

    clase de hoy

---
+++ 2025-2026/Python/Week_5/2025-11-04/dice_game1.py
@@ -0,0 +1,15 @@
+#Yofreilin
+#2025-11-04
+
+import random
+def roll_die_simulation():
+
+    num_rolls_str = "6"
+    print(f"How many times would you like to roll a single sided die? {num_rolls_str}")
+    num_rolls = int(num_rolls_str)
+    for _ in range(num_rolls):
+        roll_result = random.randint(1, 6)
+        print(f"You rolled a {roll_result}!")
+roll_die_simulation()
+
+
